52197b18f115100776ad6fc61ba1646d7a63c01b
Rahim2725 <info.abdurrahim12@gmail.com>
2022-06-10 01:48:57 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -12,12 +12,28 @@ require('dotenv').config()
 app.use(cors());
 app.use(express.json());
 
+
 //add mongo db 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ydklm.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        serverApi: ServerApiVersion.v1
+    });
 
 
-const uri = `mongodb+srv://${process.env.BD_USER}:${process.env.DB_PASSWORD}@cluster0.ydklm.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+    try {
+        await client.connect();
+        const serviceCollection = client.db("cleanCo").collections("service")
+        console.log('mongo db connected')
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
 
 
 // how to send hello message 
